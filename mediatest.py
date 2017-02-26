79be2a31b5cc7d547a0b3c1c79783ad32068d8fb
Brodi Elwood <bdelwood@mit.edu>
2017-02-26 03:04:07 -0500

    Found bug number 2

--- mediatest.py
+++ mediatest.py
@@ -47,34 +47,31 @@ from pymediainfo import MediaInfo
 import pipes
 MEDIADIR = args.in_dir
 media_info = MediaInfo.parse(MEDIADIR)
-#print 'tracks:', media_info.tracks
+print 'tracks:', media_info.tracks
 subs = []
 for track in media_info.tracks:
-    print track
-    data = track.to_data()
-    print data["track_type"]
     if data['track_type'] == 'Text' and data['language'] == lang:
         subs.append(data)
-    print subs
+print subs
         
 #   Sort list by size of track file
-    subs.sort(key=lambda sub: sub['stream_size'], reverse = True)
+subs.sort(key=lambda sub: sub['stream_size'], reverse = True)
 
 #   Main language subtitle assumed to be largest
-    main_sub = subs[0]
-    main_subsize = main_sub['stream_size']
-    main_sublen = float(main_sub['duration'])
+main_sub = subs[0]
+main_subsize = main_sub['stream_size']
+main_sublen = float(main_sub['duration'])
 #   Checks other subs for size, duration, and if forced flag is set
-    for sub in subs[1:]:
-        if (
-            sub['stream_size'] <= main_subsize*.1
-            and main_sublen*.9 <= float(sub['duration']) <= main_sublen*1.1
-            and sub['forced']=='No'
-            ):
-            secondary_sub = sub
-        else:
-            log.info("No foreign language subtitle found, try adjusting ratio.")
-    print secondary_sub['track_id']
+for sub in subs[1:]:
+    if (
+        sub['stream_size'] <= main_subsize*.1
+        and main_sublen*.9 <= float(sub['duration']) <= main_sublen*1.1
+        and sub['forced']=='No'
+        ):
+        secondary_sub = sub
+    else:
+        log.info("No foreign language subtitle found, try adjusting ratio.")
+print secondary_sub['track_id']
 
 
 #if track is not None:
