b6b526819993662e7a7d16e10163e42aad84e80a
Brodi <bdelwood@mit.edu>
2017-02-28 01:53:23 -0500

    shlex bug

--- classes/mediainfo.py
+++ classes/mediainfo.py
@@ -115,7 +115,7 @@ class ForcedSubs(object):
 
         MEDIADIR = os.path.join(dbvideo.path, dbvideo.filename)
         cmd_raw = 'mkvpropedit {} --edit track:{} --set flag-forced=1'.format(MEDIADIR, track)
-        cmd = shlex.subprocesslit(cmd_raw)
+        cmd = shlex.split(cmd_raw)
 
         proc = subprocess.Popen(
                                 cmd,
