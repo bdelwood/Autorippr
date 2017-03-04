11c7bbc70393bfe7fee1e3561312b2ff26c1b4d7
Brodi <bdelwood@mit.edu>
2017-03-04 13:44:53 -0500

    Removed personalization
    removed --format {plex}

--- classes/filebot.py
+++ classes/filebot.py
@@ -53,9 +53,7 @@ class FileBot(object):
                 '--db',
                 '%s' % db,
                 '--output',
-                "%s" % movePath,
-                '--format',
-                "{plex}"
+                "%s" % movePath
             ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
